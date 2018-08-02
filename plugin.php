4a34dbc486db84e01fe81eb67fd69589ad6e42ca
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-08-02 12:07:04 +0200

    Move widget files to separate directory.

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name:  YAFookPageBox
-Domain Path:  /
+Domain Path:  /widget
 Plugin URI:   https://github.com/TomaszGasior/YAFookPageBox
 Description:  Yet Another FacebOOK Page Plugin for WordPress. Provides sidebars widget and standalone PHP class for developers.
 Version:      2018-07-31
@@ -13,7 +13,7 @@ License:      WTFPL
 namespace YAFookPageBox;
 
 include __DIR__ . '/FacebookPageBox.php';
-include __DIR__ . '/YAFookWidget.php';
+include __DIR__ . '/widget/YAFookWidget.php';
 
 add_action('widgets_init', function(){
 	register_widget(YAFookWidget::class);
@@ -23,7 +23,7 @@ function __($text)
 {
 	static $loaded = false;
 	if (!$loaded) {
-		load_plugin_textdomain('YAFookPageBox', FALSE, basename(__DIR__));
+		load_plugin_textdomain('YAFookPageBox', FALSE, basename(__DIR__) . '/widget');
 		$loaded = true;
 	}
 
